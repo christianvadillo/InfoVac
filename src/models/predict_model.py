0d1b082a0db931119b82aefce19f828d43ca4af2
christian_vadillo <christianvadillo@hotmail.com>
2020-11-14 19:44:46 -0500

    train, validate, deploy

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -5,7 +5,7 @@ from typing import List
 file = '../../models/svm_pipeline_10_Nov_2020_10_30.sav'
 
 
-def is_list_of_strings(lst):
+def is_list_of_strings(lst: List) -> bool:
     if lst and isinstance(lst, list):
         return all(isinstance(elem, str) for elem in lst)
     else:
@@ -19,4 +19,7 @@ def predict(text: List[str]):
 
 
 if __name__ == '__main__':
-    predict(['Esto es un texto de prueba para nuestro clasificador svm para detectar información poco confiable'])
+    predict(['Esto es un texto de prueba para nuestro clasificador svm para detectar información poco confiable',
+             'Una receta con aspirinas, jengibre, canela, limón, cebolla y miel cura el COVID-19',
+             "Circularon en redes sociales supuestas declaraciones del actor argentino sobre el caso de la joven cordobesa que no pudo ver a su padre antes de morir de cáncer en Córdoba, debido a las medidas de restricción de la pandemia. ”La chica pudo ver a su padre por wassap. No es que no la vio” (sic), es la frase que se le atribuye a Rizzo y que es falsa, ya que no hay registros públicos de que haya hecho tales afirmaciones. ",
+            ])
